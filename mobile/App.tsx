aca29bc40e6e206d490149372ee07bf81f787c4f
MedYassineGuembri <medyassineguembri@gmail.com>
2024-07-03 16:56:18 +0200

    feat: card detection

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,129 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import LandingScreen from './src/screens/LandingScreen';
 import ConnectScreen from './src/screens/ConnectScreen';
+
 import {RootStackParamList} from './src/types/navigation';
+import {Image, StyleSheet} from 'react-native';
 
 const Tab = createBottomTabNavigator<RootStackParamList>();
 
 function App() {
+  const [isScanning, setIsScanning] = useState<boolean>(false);
+  const [isScanned, setIsScanned] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [decoded, setDecoded] = useState<any>(null);
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Landing" component={LandingScreen} />
-        <Tab.Screen name="Connect" component={ConnectScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            headerShown: false,
+            tabBarIcon: ({focused}) => {
+              let iconComponent;
+              if (route.name === 'Scan') {
+                iconComponent = focused ? (
+                  <Image
+                    source={require('./src/assets/scanIconYellow.png')}
+                    style={styles.scanIcon}
+                  />
+                ) : (
+                  <Image
+                    source={require('./src/assets/scanIconWhite.png')}
+                    style={styles.scanIcon}
+                  />
+                );
+              } else if (route.name === 'Connect') {
+                iconComponent = focused ? (
+                  <Image
+                    source={require('./src/assets/connectIconYellow.png')}
+                    style={styles.connectIcon}
+                  />
+                ) : (
+                  <Image
+                    source={require('./src/assets/connectIconWhite.png')}
+                    style={styles.connectIcon}
+                  />
+                );
+              }
+              return iconComponent;
+            },
+            tabBarActiveTintColor: '#D5A419',
+            tabBarInactiveTintColor: '#858CA7',
+            tabBarStyle: [
+              styles.tabBar,
+              isScanned && decoded
+                ? styles.scannedTabBar
+                : isError
+                ? styles.errorTabBar
+                : null,
+            ],
+            tabBarLabelStyle: {
+              fontSize: 12,
+              fontWeight: 'bold',
+              marginBottom: 10,
+            },
+          })}>
+          <Tab.Screen name="Scan">
+            {props => (
+              <LandingScreen
+                {...props}
+                setIsScanning={setIsScanning}
+                setIsScanned={setIsScanned}
+                setDecoded={setDecoded}
+                setIsError={setIsError}
+                decoded={decoded}
+                isError={isError}
+                isScanned={isScanned}
+                isScanning={isScanning}
+              />
+            )}
+          </Tab.Screen>
+          <Tab.Screen name="Connect">
+            {props => (
+              <ConnectScreen
+                {...props}
+                setIsScanning={setIsScanning}
+                setIsError={setIsError}
+                setIsScanned={setIsScanned}
+                setDecoded={setDecoded}
+              />
+            )}
+          </Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  connectIcon: {
+    width: 50,
+    height: 50,
+  },
+  scanIcon: {
+    width: 21,
+    height: 30,
+  },
+  tabBar: {
+    backgroundColor: '#252C40',
+    display: 'flex',
+    height: 70,
+    borderColor: '#252C40',
+    elevation: 0,
+    shadowOpacity: 0,
+  },
+  scannedTabBar: {
+    backgroundColor: '#00C6BF',
+    borderColor: '#00C6BF',
+  },
+  errorTabBar: {
+    backgroundColor: '#E73A14',
+    borderColor: '#E73A14',
+  },
+});
 
 export default App;
--- mobile/src/screens/LandingScreen.tsx
+++ mobile/src/screens/LandingScreen.tsx
@@ -1,24 +1,68 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet, Image, Animated} from 'react-native';
 import NfcManager, {NfcTech, Ndef} from 'react-native-nfc-manager';
+import {jwtDecode} from 'jwt-decode';
 import {LandingScreenNavigationProp} from '../types/navigation.ts';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
 type Props = {
   navigation: LandingScreenNavigationProp;
+  setIsScanning: (value: boolean) => void;
+  setIsScanned: (value: boolean) => void;
+  setIsError: (value: boolean) => void;
+  setDecoded: (value: any) => void;
+  isScanning: boolean;
+  isScanned: boolean;
+  isError: boolean;
+  decoded: any;
 };
-const LandingScreen: React.FC<Props> = ({navigation}) => {
+
+const LandingScreen: React.FC<Props> = ({
+  navigation,
+  isScanning,
+  isScanned,
+  isError,
+  decoded,
+  setIsScanning,
+  setIsError,
+  setIsScanned,
+  setDecoded,
+}) => {
   const [tagData, setTagData] = useState<string | null>(null);
-  const [isScanning, setIsScanning] = useState<boolean>(false);
+
+  const [animationOpacity] = useState(new Animated.Value(1));
+
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
-      setIsScanning(false);
-      setTagData(null);
+      cancelReadNfc();
     });
 
     return unsubscribe;
   }, [navigation]);
 
+  useEffect(() => {
+    if (isScanning) {
+      startAnimation();
+    } else {
+      animationOpacity.setValue(1);
+      animationOpacity.stopAnimation();
+    }
+  }, [isScanning]);
+
+  const cancelReadNfc = async () => {
+    setIsScanning(false);
+    setIsScanned(false);
+    setTagData(null);
+    setDecoded(null);
+  };
+  const cancelError = async () => {
+    setIsError(false);
+  };
+
   const readNfcTag = async () => {
-    setIsScanning(true);
+    if (!isError) {
+      setIsScanning(true);
+    }
     try {
       await NfcManager.requestTechnology(NfcTech.Ndef);
       const tag = await NfcManager.getTag();
@@ -28,43 +72,150 @@ const LandingScreen: React.FC<Props> = ({navigation}) => {
           const payload = new Uint8Array(ndefRecord.payload);
           const text = Ndef.text.decodePayload(payload);
           setTagData(text);
-          setIsScanning(false);
+          setIsError(false);
+          setIsScanned(true);
+          const decodedtext: any = jwtDecode(text);
+          setDecoded(decodedtext);
         } else {
+          cancelReadNfc();
+          setIsError(true);
+          readNfcTag();
           setTagData('No NDEF message payload found');
         }
       } else {
+        cancelReadNfc();
+        setIsError(true);
+        readNfcTag();
         setTagData('No NDEF message found');
       }
     } catch (ex) {
-      console.warn(ex);
+      cancelReadNfc();
+      setIsError(true);
+      readNfcTag();
       setTagData('Error reading NFC tag');
     } finally {
       NfcManager.cancelTechnologyRequest();
     }
   };
 
+  const startAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(animationOpacity, {
+          toValue: 0.1,
+          duration: 800,
+          useNativeDriver: true,
+        }),
+        Animated.timing(animationOpacity, {
+          toValue: 1,
+          duration: 800,
+          useNativeDriver: true,
+        }),
+      ]),
+      {iterations: -1},
+    ).start();
+  };
+
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        isScanned && decoded ? styles.scannedContainer : null,
+        isError ? styles.errorContainer : null,
+      ]}>
       <Image
         source={require('../assets/teki-logo.png')}
-        style={styles.imageStyl}></Image>
+        style={styles.imageStyle}
+      />
       {isScanning ? (
-        <View>
-          <Text style={styles.scanningTitle}>Scanning ...</Text>
-          <Text style={styles.underText}>
-            Keep your card close to your phone...
-          </Text>
-        </View>
-      ) : (
+        isScanned ? (
+          <View>
+            {decoded && (
+              <View>
+                <Text style={styles.title}>{decoded.name}</Text>
+                <Text
+                  style={[
+                    styles.underText,
+                    isScanned && decoded ? styles.scannedUnderText : null,
+                  ]}>
+                  Thanks ! Your card is valid
+                </Text>
+              </View>
+            )}
+          </View>
+        ) : !isError ? (
+          <View>
+            <Text style={styles.scanningTitle}>Scanning ...</Text>
+            <Text style={styles.underText}>
+              Keep your card close to your phone...
+            </Text>
+          </View>
+        ) : null
+      ) : !isError ? (
         <View>
           <Text style={styles.title}>Scan your card</Text>
           <Text style={styles.underText}>
             Authenticate with your physical card
           </Text>
         </View>
-      )}
-      <Button title="Scan NFC Tag" onPress={readNfcTag} />
-      {tagData && <Text style={styles.text}>Tag Data: {tagData}</Text>}
+      ) : null}
+      {isError ? (
+        <View>
+          <Text style={styles.title}>Unknown card</Text>
+          <Text
+            style={[
+              styles.underText,
+              isError ? styles.scannedUnderText : null,
+            ]}>
+            Please try scanning again
+          </Text>
+        </View>
+      ) : null}
+
+      <View style={styles.centeredButtonContainer}>
+        {isScanning ? (
+          isScanned ? (
+            <View>
+              {decoded && (
+                <Image
+                  source={require('../assets/tick.png')}
+                  style={styles.scanbuttonImage}
+                />
+              )}
+            </View>
+          ) : !isError ? (
+            <Animated.View style={{opacity: animationOpacity}}>
+              <TouchableOpacity onPress={cancelReadNfc}>
+                <Image
+                  source={require('../assets/scanning.png')}
+                  style={styles.scanningbuttonImage}
+                />
+              </TouchableOpacity>
+            </Animated.View>
+          ) : (
+            <TouchableOpacity onPress={cancelError}>
+              <Image
+                source={require('../assets/x.png')}
+                style={styles.scanbuttonImage}
+              />
+            </TouchableOpacity>
+          )
+        ) : !isError ? (
+          <TouchableOpacity onPress={readNfcTag}>
+            <Image
+              source={require('../assets/scan.png')}
+              style={styles.scanbuttonImage}
+            />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={cancelError}>
+            <Image
+              source={require('../assets/x.png')}
+              style={styles.scanbuttonImage}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 };
@@ -74,30 +225,58 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#252C40',
   },
+  scannedContainer: {
+    backgroundColor: '#00C6BF',
+  },
+  errorContainer: {
+    backgroundColor: '#E73A14',
+  },
   title: {
     marginLeft: 20,
-    fontSize: 24,
-    fontFamily: 'Inter-Bold',
+    fontSize: 30,
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
     color: '#fff',
   },
   underText: {
     marginLeft: 20,
-    fontSize: 13,
+    fontSize: 16,
     marginBottom: 20,
     color: '#858CA7',
+    fontWeight: '500',
+  },
+  scannedUnderText: {
+    color: '#252C40',
   },
   scanningTitle: {
-    fontSize: 24,
-
+    fontSize: 30,
     marginLeft: 20,
     color: '#D5A419',
+    fontWeight: 'bold',
   },
   text: {
     marginTop: 20,
+    color: '#fff',
   },
-  imageStyl: {
+  centeredButtonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageStyle: {
+    marginTop: 20,
     width: 150,
     height: 150,
+  },
+  scanbuttonImage: {
+    marginBottom: 40,
+    width: 150,
+    height: 150,
+  },
+  scanningbuttonImage: {
+    marginBottom: 50,
+    width: 250,
+    height: 250,
   },
 });
 
